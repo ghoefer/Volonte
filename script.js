9f80df00713647e530c8fc1b4bfd6e5d09074dd5
Gabby Hoefer <ghoefer@samford.edu>
2018-09-21 14:45:44 -0500

    login form update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 alert ("This is under construction")
 
-$('.message a').click(function(){
+$('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+   function myFunction() {
+    document.getElementsByTagName("login-overall")[0].style.backgroundColor = "yellow";
+
 });
 
 
