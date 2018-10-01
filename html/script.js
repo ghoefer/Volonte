e329197ab32895db45a76e019f88665f64cf4819
Gabby Hoefer <ghoefer@samford.edu>
2018-10-01 00:13:44 -0500

    form

--- html/script.js
+++ html/script.js
@@ -1,5 +1,5 @@
 
-alert ("This is under construction")
+alert ("This is under construction");
 
 $('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
@@ -7,28 +7,3 @@ $('.message').click(function(){
     document.getElementsByTagName("login-overall")[0].style.backgroundColor = "yellow";
 
 });
-
-
-<form name="loginform" onSubmit="return validateForm();" action="form.html" method="post">
-    <label>User name</label>
-    <input type="text" name="usr" placeholder="username">
-    <label>Password</label>
-    <input type="password" name="pwrd" placeholder="password">
-    <input type="submit" value="Loginbtn"/>
-</form>
-
-<script>
-    function validateForm() {
-        var un = document.loginform.usr.value;
-        var pw = document.loginform.pword.value;
-        var username = "username";
-        var password = "password";
-        if ((un == username) && (pw == password)) {
-            return true;
-        }
-        else {
-            alert ("Login was unsuccessful, please check your username and password");
-            return false;
-        }
-  }
-</script>
